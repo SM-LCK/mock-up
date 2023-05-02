5bfc6772e22818e257986eb08a0ca3d0423e8740
kelly1422 <kelly1422@naver.com>
2023-05-03 04:18:38 +0900

    네비게이션 route -> no pot, 탭바 구성

--- lib/screens/routelist.dart
+++ lib/screens/routelist.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practice/screens/no_pot.dart';
 
 
 class RouteListPage extends StatelessWidget {
@@ -7,31 +8,130 @@ class RouteListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage('assets/charge.png'),
-              )
-          ),
-          child: Scaffold(
-            backgroundColor: Colors.transparent,
-            body: GestureDetector(
-              onTap: () {
-              },
-              child: Padding(
-                padding: const EdgeInsets.only(left: 55, top: 560),
-                child: Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                          width: 5,
-                          color: Colors.blue
-                      )
+        home: Scaffold(
+          body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/route.png'),
+                )
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  height: 370,
+                  padding: const EdgeInsets.only(top: 150),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context){
+                        return const NoPotPage();
+                      }));
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              width: 5,
+                              color: Colors.blue
+                          )
+                      ),
+                      width: 360,
+                      height: 170,
+                    ),
                   ),
-                  width: 300,
-                  height: 80,
                 ),
-              ),
+                Container(
+                  alignment: Alignment.topCenter,
+                  height: 270,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context){
+                        return const NoPotPage();
+                      }));
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              width: 5,
+                              color: Colors.blue
+                          )
+                      ),
+                      width: 360,
+                      height: 170,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                width: 5,
+                                color: Colors.blue
+                            )
+                        ),
+                        width: 100,
+                        height: 100,
+                        child: Icon(
+                            Icons.alt_route,
+                            color: Colors.black,
+                            size : 40
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context){
+                          return const NoPotPage();
+                        }));
+                      },
+                      child: Container(
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  width: 5,
+                                  color: Colors.blue
+                              )
+                          ),
+                          width: 100,
+                          height: 100,
+                          child: Text(
+                            "참여중",
+                            style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                          )
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context){
+                          return const NoPotPage();
+                        }));
+                      },
+                      child: Container(
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  width: 5,
+                                  color: Colors.blue
+                              )
+                          ),
+                          width: 100,
+                          height: 100,
+                          child:
+                          Text(
+                            "MY",
+                            style: const TextStyle(fontSize: 40),
+                          )
+                      ),
+                    )
+                  ],
+                )
+              ],
             ),
           ),
         )
