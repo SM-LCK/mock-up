a9ab358f07459db6734b696a7232e8fe1b3a2f41
kelly1422 <kelly1422@naver.com>
2023-05-04 22:30:23 +0900

    myPage 루트 추가

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:practice/screens/n_1request.dart';
 import 'package:practice/screens/price.dart';
 import 'package:practice/screens/record.dart';
 import 'package:practice/screens/routelist.dart';
+import 'package:practice/screens/createpot.dart';
 
 void main() {
   runApp(MyApp()); // 앱 실행 MyApp에 메인페이지 적어주면 됨
@@ -26,8 +27,9 @@ class MyApp extends StatelessWidget {
         '/IngPage':(context)=>IngPage(),
         '/ChargePage': (context)=>ChargePage(),
         '/RecordPage': (context)=>RecordPage(),
-        '/NrequestPage':(context)=>NrequestPage(),
+        '/NrequestPage':(context)=>N_1RequestPage(),
         '/PricePage':(context) => PricePage(),
+        '/CreatePotPage':(context) => CreatePotPage()
       },
       //  home: Scaffold(
       //    body: SafeArea(
